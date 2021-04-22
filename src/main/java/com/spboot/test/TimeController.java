d66f58ad507cd35cc600e6fea70e37dc124ec869
Rion Park <redfilaruntime@gmail.com>
2021-04-22 16:04:34 +0900

    시간 테스트

---
+++ src/main/java/com/spboot/test/TimeController.java
@@ -0,0 +1,27 @@
+package com.spboot.test;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.Data;
+
+@Data
+class ThisDate{
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private Date date;
+}
+@RestController
+public class TimeController {
+
+	@GetMapping("/date")
+	public ThisDate getDate() {
+		ThisDate td = new ThisDate();
+		td.setDate(new Date());
+		return td;
+	}
+}
