91e67a79dc98834240bbc18c7b9a99fe3303d5e5
Rion Park <redfilaruntime@gmail.com>
2021-04-23 10:21:19 +0900

    http to https setting

--- src/main/java/com/spboot/test/TimeController.java
+++ src/main/java/com/spboot/test/TimeController.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
 class ThisDate{
@@ -16,8 +17,10 @@ class ThisDate{
 	private Date date;
 }
 @RestController
+@Slf4j
 public class TimeController {
 
+	
 	@GetMapping("/date")
 	public ThisDate getDate() {
 		ThisDate td = new ThisDate();
---
+++ src/main/java/com/spboot/test/bean/TestBean.java
@@ -0,0 +1,8 @@
+package com.spboot.test.bean;
+
+public class TestBean {
+
+	public String getStr() {
+		return "이 클래스는 testBean 입니다.";
+	}
+}
